4813efee7ca5008f6f2914bb74ecf928af1ab1fb
OhHyeonJu0415 <guswn2209@gmail.com>
2022-11-11 17:36:58 +0900

    refactor : 책 덮기 호출 방식 변경

--- src/App.js
+++ src/App.js
@@ -7,14 +7,49 @@ import Foundation from "./scripts/books/foundation";
 import Senior from "./scripts/books/senior.js";
 import { setBook } from "./store/modules/book";
 import { useDispatch, useSelector } from "react-redux";
+import Arrow from "./scripts/common/arrow";
 
 const App = () => {
   const book = useSelector((state) => state.book);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   return (
     <div className={styles.container}>
       {console.log(book)}
+      {book.book !== "default" ? ( //책 덮기
+        <div
+          id="prologueArrow"
+          className={styles.arrow}
+          onClick={(e) => {
+            //이벤트 버블링/캡쳐링 막기
+            e.preventDefault();
+            e.stopPropagation();
+
+            //리덕스 값 변경
+            dispatch(setBook("default"));
+
+            //책 복구하기
+            //프롤로그 책 올리기
+            document
+              .getElementById("prologue")
+              .style.removeProperty("transform");
+
+            //나머지 책 내리기
+            document
+              .getElementById("sophomore")
+              .style.removeProperty("transform");
+            document.getElementById("junior").style.removeProperty("transform");
+            document
+              .getElementById("foundation")
+              .style.removeProperty("transform");
+            document.getElementById("senior").style.removeProperty("transform");
+          }}
+        >
+          <Arrow />
+        </div>
+      ) : (
+        <></>
+      )}
       <Prologue />
       <Sophomore />
       <Junior />
